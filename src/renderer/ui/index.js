dba8ad4fe1b4d1f3e28d73ccfaf311d4d07bb638
YanisB. <77593289+Sterll@users.noreply.github.com>
2026-02-08 12:00:32 +0100

    refactor(ui): export terminal themes from ui module

--- src/renderer/ui/index.js
+++ src/renderer/ui/index.js
@@ -3,8 +3,11 @@
  */
 
 const components = require('./components');
+const themes = require('./themes/terminal-themes');
 
 module.exports = {
   components,
-  ...components
+  ...components,
+  themes,
+  ...themes
 };
